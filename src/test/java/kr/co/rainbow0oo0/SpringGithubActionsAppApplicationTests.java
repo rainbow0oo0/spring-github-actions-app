6bcf76878d1c7930597edadb2f72fc353deb7f83
rainobow0oo0 <eternalsoul0634@gmail.com>
2025-09-26 10:11:14 +0900

    ?

---
+++ src/test/java/kr/co/rainbow0oo0/SpringGithubActionsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.rainbow0oo0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGithubActionsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
